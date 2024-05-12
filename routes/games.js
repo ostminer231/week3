87b51f43a9df9b01e01dc43a316a9e38b52d5edb
miner212321321 <117258266+ostminer231@users.noreply.github.com>
2024-05-12 23:48:21 +0500

    На проверку

--- routes/games.js
+++ routes/games.js
@@ -1,10 +1,31 @@
-// routes/games.js
-const gamesRouter = require("express").Router(); // Создали роутер
-const { sendAllGames, deleteGame, addGameController } = require("../controllers/games");
-const { getAllGames } = require("../middlewares/games");
-
-gamesRouter.get("/games", getAllGames, sendAllGames)
-gamesRouter.delete("/games/:id", getAllGames, deleteGame);
-gamesRouter.post("/games", getAllGames, addGameController)
-
-module.exports = gamesRouter;
+const {
+    getAllGames,
+    checkIsTitleInArray,
+    updateGamesArray,
+    updateGamesFile,
+    findGameById,
+    deleteGame
+  } = require("../middlewares/games");
+  const { sendAllGames, sendUpdatedGames } = require("../controllers/games");
+  
+  gamesRouter.get("/games", getAllGames, sendAllGames);
+  
+  gamesRouter.post(
+    "/games",
+    getAllGames,
+    checkIsTitleInArray,
+    updateGamesArray,
+    updateGamesFile,
+    sendUpdatedGames
+  );
+  
+  gamesRouter.delete(
+    "/games/:id",
+    getAllGames,
+    findGameById,
+    deleteGame,
+    updateGamesFile,
+    sendUpdatedGames
+  );
+  
+  module.exports = gamesRouter; 
