aba95a1d5d2466592d32ff6965223b0f4a9ba289
miner212321321 <117258266+ostminer231@users.noreply.github.com>
2024-05-12 23:11:17 +0500

    Проект

---
+++ middlewares/cors.js
@@ -0,0 +1,20 @@
+const allowedCors = [
+    'https://practicum.yandex.ru',
+    'https://students-projects.ru',
+    'http://localhost:3000'
+  ]; 
+
+function cors(req, res, next) {
+    const { origin } = req.headers;
+    
+    if (allowedCors.includes(origin)) { // Если это наш друг
+        //res.header('Access-Control-Allow-Origin', origin);
+        res.header('Access-Control-Allow-Origin', '*'); // Открываем доступ всем 
+    }
+    
+    next();
+}
+
+module.exports = {
+    cors
+}
